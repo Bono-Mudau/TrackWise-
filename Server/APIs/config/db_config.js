11abd86f5781800002ced5c391acbf49db200877
Bono-Mudau <bonomudau6@gmail.com>
2026-03-10 15:27:18 +0200

    Fixied database_config error

--- Server/APIs/config/db_config.js
+++ Server/APIs/config/db_config.js
@@ -18,9 +18,17 @@ CREATE TABLE IF NOT EXISTS users (
   user_id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(300) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 `;
+
+db.query("SELECT 1", (err, res) => {
+  if (err) {
+    console.error("DB connection failed:", err);
+  } else {
+    console.log("DB connected successfully");
+  }
+});
 
 db.query(createUsersTable, (err, results) => {
   if (err) throw err;
