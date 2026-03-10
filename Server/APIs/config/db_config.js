aa372ddfa3018c171cb80f09d19bc1bba4923a5d
Bono-Mudau <bonomudau6@gmail.com>
2026-03-10 17:58:10 +0200

    Adding tables to the database

--- Server/APIs/config/db_config.js
+++ Server/APIs/config/db_config.js
@@ -14,12 +14,46 @@ const db = mysql.createPool({
 });
 console.log("DB connected");
 const createUsersTable = `
-CREATE TABLE IF NOT EXISTS users (
-  user_id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(300) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
+create table expenses(
+  exp_id int auto_increment primary key,
+  date_created datetime default current_timestamp,
+  description  varchar(300) ,
+  category varchar(255),
+  amount decimal(10,2) not null,
+  status tinyint(1) default 0,
+  due_date datetime,
+  user_id int not null
+ );
+`;
+const createUsersTable1 = `
+ create table income(
+  income_id int auto_increment primary key,
+  date datetime default current_timestamp,
+  category varchar(255),
+  amount decimal(10,2) not null,
+  user_id int not null
+ );
+`;
+const createUsersTable2 = `
+
+ create table monthly_summary(
+  id int auto_increment primary key,
+  income decimal(11,20) default 0.00,
+  income decimal(11,20) default 0.00,
+  year int not null,
+  month int not null,
+  user_id int not null
+ );
+`;
+const createUsersTable3 = `
+
+ create table otp(
+  id int auto_increment primary key,
+  email varchar(255) not null,
+  otp varchar(6) not null,
+  expiry_time datetime not null
+  );
+
 `;
 
 db.query("SELECT 1", (err, res) => {
@@ -34,5 +68,20 @@ db.query(createUsersTable, (err, results) => {
   if (err) throw err;
   console.log('Users table created!');
 });
+db.query(createUsersTable1, (err, results) => {
+  if (err) throw err;
+  console.log('Users1 table created!');
+});
+
+db.query(createUsersTable2, (err, results) => {
+  if (err) throw err;
+  console.log('Users2 table created!');
+});
+
+db.query(createUsersTable3, (err, results) => {
+  if (err) throw err;
+  console.log('Users3 table created!');
+});
+
 
 module.exports=db;
